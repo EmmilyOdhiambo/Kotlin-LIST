2e9180bcd93a3e3f3651d9b8a263cee59bf748d1
EmmilyOdhiambo <emmilysodhiambo@gmail.com>
2023-03-29 19:28:10 +0300

    List

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,22 @@ fun main() {
     var typ= listOf("Steph","Emmie","Joy","Maggy")
     println(typ)
 
+    person()
+    var people1=People("Maggy",45,30.5,55)
+    var people2=People("Emmie",10,55.6,50)
+    var people3=People("John",12,56.6,30)
+    var people4=People("Faith",40,45.5,45)
+
+    var people= listOf<People>(people1,people2,people3,people4)
+    var pple=people.sortedByDescending {people->people.age }
+    println(pple)
+    var one = Vehicles("KGR",345)
+    var two = Vehicles("OPY",106)
+    var three = Vehicles("GTR",570)
+    var four = Vehicles("SDR",875)
+    var allVehicles= listOf(one,two,three,four)
+    println(vehicleList(allVehicles))
+
 
 }//1. Given a list of 10 strings, write a function that returns a list of the strings
 //at even indices i.e index 2,4,6 etc (2 points)
@@ -27,17 +43,21 @@ class People (var name:String,var age:Int,var height:Double,var weight:Int)
 //4. Given a list similar to the one above, write a function in which you will
 //create 2 more people objects and add them to the list at one go.
 //(1 points)
-fun addperson(topple:List<People>):List<Any>{
+fun addperson(topple:List<People>):List<Any> {
     return listOf()
-
+}
 //5. Write a function that takes in a list of Car objects each with a
 //registration and mileage attribute and returns the average mileage of
 //all the vehicles in the list./
-class car(var register:String,var mileage:Int)
-fun classList(vehicles:List<car>): Int{
-    var avg = 0
-    vehicles.forEach{ new -> new.mileage
-    avg.}
-}
+data class Vehicles(var registration:String, var mileage:Int)
+    fun vehicleList(vehicle:List<Vehicles>):Int {
+        var avg = 0
+        vehicle.forEach { new ->
+            new.mileage
+            avg += new.mileage
+        }
+        var totalAvg = vehicle.count()
+        return totalAvg
+    }
 
 
